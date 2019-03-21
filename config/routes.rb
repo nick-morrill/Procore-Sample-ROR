9ee85caa920803c1012205c74e97e1efe1ff881e
Geoff Hull <geoffhull03@gmail.com>
2019-03-21 13:51:48 -0700

    Stopping Point for check-in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'login/callback'
 
+  post 'login/refresh'
+
   root 'login#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
