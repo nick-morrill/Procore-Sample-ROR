78fbacf6010f41dea338712beefbf762a1fcbebe
Nick Morrill <nick.morrill@procore.com>
2019-05-29 11:38:54 -0700

    Removed setting of environment variables to global variables from application_controller.rb. Added hyperlink to README.md. Updated styling of login_controller.rb and users_controller.rb. Added explanatory comments to controller files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  CLIENT_ID = ENV['CLIENT_ID']
-  CLIENT_SECRET = ENV['CLIENT_SECRET']
-  REDIRECT_URI = ENV['REDIRECT_URI']
-  OAUTH_URL = ENV['OAUTH_URL']
-  BASE_URL = ENV['BASE_URL']
 end
